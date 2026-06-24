c1c8723cebc21b7e8b3461052f7ba1e340a68c99
Anjaan Banjara <luckybanjara.anjaan@gmail.com>
2026-06-24 22:53:56 +0800

    feat(array of objects): made sure to learn array of objects

--- my-react-app/src/MyObject.jsx
+++ my-react-app/src/MyObject.jsx
@@ -38,32 +38,85 @@ function MyObject(){
 
 //  update Arrays in React
 
-const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+// const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
 
-function handleAddFood(){
+// function handleAddFood(){
     
-    const newFood = document.getElementById("foodInput").value;
-    document.getElementById("foodInput").value = "";
+//     const newFood = document.getElementById("foodInput").value;
+//     document.getElementById("foodInput").value = "";
 
-    setFoods (f => ([...f,newFood])) ;
+//     setFoods (f => ([...f,newFood])) ;
 
+// }
+
+// function handleRemoveFood(index){
+//  setFoods(foods.filter((_, i) => i !== index ));
+
+// }
+
+// return( <div>
+//         <h2>List of Food</h2>
+//         <ul>
+//         {foods.map((food, index)=> <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+//         </ul>
+//         <input type="text" id="foodInput" placeholder="Enter food name"/>
+//         <button onClick={handleAddFood}>Add Food</button>
+//         </div>
+
+// );}
+
+
+
+
+// update array of objects
+
+const [cars, setCars] = useState([]);
+const [carYear, setcarYear] = useState(new Date().getFullYear());
+const [carMake, setcarMake] = useState("");
+const [carModel, setcarModel] =useState("");
+
+function handleAddCar(){
+   const newCar = {year: carYear,
+                   make: carMake,
+                   model:carModel};
+    setCars(c => [...c, newCar]);
+
+    setcarYear(new Date().getFullYear());
+    setcarMake("");
+    setcarModel("");
 }
 
-function handleRemoveFood(index){
- setFoods(foods.filter((_, i) => i !== index ));
+function handleRemoveCar(index){
+    setCars(c => c.filter((_, i) => i!==index));
 
 }
+function handleYearChange(event){
+    setcarYear(event.target.value);
 
-return( <div>
-        <h2>List of Food</h2>
-        <ul>
-        {foods.map((food, index)=> <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
-        </ul>
-        <input type="text" id="foodInput" placeholder="Enter food name"/>
-        <button onClick={handleAddFood}>Add Food</button>
-        </div>
+}
+function handleMakeChange(event){
+    setcarMake(event.target.value);
+}
+function handleModelChange(event){
+    setcarModel(event.target.value);
+}
 
+
+return (
+      <div>
+       <h2>List of Car Objects</h2>
+       <ul>
+        {cars.map((car, index) => 
+        <li key={index} onClick={()=> handleRemoveCar(index)}>
+         {car.year} {car.make} {car.model}
+        </li> )}
+       </ul>
+       <input type="number" value={carYear} onChange={handleYearChange}/> <br />
+       <input type="text"  value={carMake} onChange={handleMakeChange} placeholder="Enter car make"/> <br />
+       <input type="text"  value={carModel} onChange={handleModelChange} placeholder="Enter car model"/> <br />
+       <button onClick={handleAddCar}>Add Car</button>
+      </div>
 );}
 
 
